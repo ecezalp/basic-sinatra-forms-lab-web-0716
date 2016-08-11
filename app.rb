e6d2c28d4459cf2e7b8efb3ae307ddf453445767
Ece Ozalp <github email address>
2016-08-11 16:53:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
+require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @params = params
+    erb :team
+  end
 
 
 end
